4a2341660ee7ce1b0f28595eb6db6d5b9fadd8cc
davidluna1995 <david.luna.cuevas@gmail.com>
2019-06-21 16:41:46 -0400

    modal ingreso y cierre mensual con su respectiva tabla

--- angular-module/src/app/auth-master/cuenta-sindical/formulario-sindical/formulario-sindical.component.ts
+++ angular-module/src/app/auth-master/cuenta-sindical/formulario-sindical/formulario-sindical.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-formulario-sindical',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioSindicalComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
+  }
+
+  openLg(content) {
+    this.modalService.open(content, { size: 'lg' });
   }
 
 }

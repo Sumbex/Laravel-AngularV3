6abfed2ad61863024b04fc61bc116f520f3dbe00
Sumbex <bmontecino.96@gmail.com>
2019-07-02 02:28:18 -0400

    avances caja chica

--- laravel-module/app/CajaChica.php
+++ laravel-module/app/CajaChica.php
@@ -3,33 +3,79 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 class CajaChica extends Model
 {
-    protected $table = "c_s_caja_chica";
+    protected $table = "cs_caja_chica";
 
     protected function ingresarCajaChica($request){
-        $caja = $this;
+        $caja = new CajaChica;
 
-        $caja->id = 5423;
         $caja->anio_id = $request->anio_id;
         $caja->mes_id = $request->mes_id;
         $caja->dia = $request->dia;
         $caja->numero_documento = $request->numero_documento;
-        $caja->archivo_documento = $request->archivo_documento;
+        $caja->archivo_documento = '/doc/archivo.pdf';
         $caja->descripcion = $request->descripcion;
-        $caja->monto_ingreso = 123;
-        $caja->monto_egreso =  321;
-        $caja->definicion = $request->definicion;
-        $caja->user_crea = $request->user_crea;
+        $caja->monto_egreso =  $request->monto_egreso;
+        $caja->definicion = 3;
+        $caja->user_crea = Auth::user()->id;
         $caja->activo = "S";
 
-        dd($caja->save());
         if ($caja->save()) {
     		return ['status'=>'success', 'mensaje'=>'Insertado'];
     	}
         
-        
     }
+
+    protected function existeCajaChica($anio, $mes){
+        //cuenta_sindicato
+        $cSindicato = DB::table('cuenta_sindicato')
+        ->select([
+            'tipo_cuenta_sindicato',
+            'monto_egreso',
+            'anio_id',
+            'mes_id'
+        ])
+        ->where([
+            'cuenta_sindicato.activo'=>'S',
+            'cuenta_sindicato.anio_id' => $anio, 
+            'cuenta_sindicato.mes_id' => $mes,
+            'cuenta_sindicato.tipo_cuenta_sindicato' => 3
+        ])
+        ->get();
+       
+        if(!$cSindicato->isEmpty()){
+            return $cSindicato[0]->monto_egreso;
+        }else{
+            return "aun no a realizado el ingreso a caja chica";
+        }
+
+    }
+
+    protected function traerCajaChica($anio, $mes){
+      
+        $caja = DB::table('cs_caja_chica as cc')
+        ->select([
+            'cc.id',
+    		DB::raw("concat(cc.dia,' de ',m.descripcion,',',a.descripcion) as fecha"),
+    		'cc.numero_documento',
+    		'cc.archivo_documento',
+    		'cc.descripcion',
+    		'cc.monto_egreso'
+        ])
+        ->join('anio as a','a.id','anio_id')
+    	->join('mes as m','m.id','mes_id')
+		->where([
+				'cc.activo'=>'S',
+				'cc.anio_id' => $anio, 
+				'cc.mes_id' => $mes,
+        ])
+        ->get();
+
+        return $caja;
+    }
+    
     
 }

58c414ca9a50c8d721e5f85736a2f85e3efa44df
Sumbex <bmontecino.96@gmail.com>
2019-07-03 18:43:02 -0400

    avance caja chica 4 creo

--- laravel-module/app/Http/Controllers/CajaChicaController.php
+++ laravel-module/app/Http/Controllers/CajaChicaController.php
@@ -8,6 +8,8 @@ use App\CajaChica;
 
 class CajaChicaController extends Controller
 {
+    public  $saldo = 100000;
+    
     public function IngresarCajaChica(Request $request){
         return CajaChica::ingresarCajaChica($request);
     }
@@ -17,6 +19,38 @@ class CajaChicaController extends Controller
     }
 
     public function TraerCajaChica($anio, $mes){
-        return CajaChica::traerCajaChica($anio, $mes);
+        $caja = CajaChica::traerCajaChica($anio, $mes);
+        $tomar = true;
+
+        for ($i=0; $i < count($caja); $i++) { 
+            switch ($caja[$i]->definicion) {
+                case 1:
+                    if($tomar == true){
+                        $caja[$i]->saldo_actual = $this->saldo + $caja[$i]->monto_ingreso;
+                        $tomar = false;
+                    }else{
+                        $caja[$i]->saldo_actual = $caja[$i-1]->saldo_actual + $caja[$i]->monto_ingreso;
+                    }
+                break;
+                case 2:
+                    if($tomar == true){
+                        $caja[$i]->saldo_actual = $this->saldo - $caja[$i]->monto_egreso;
+                        $tomar = false;
+                    }else{
+                        $caja[$i]->saldo_actual = $caja[$i-1]->saldo_actual - $caja[$i]->monto_egreso;
+                    }
+                break;
+            }
+        }
+        return $caja;
     }
+    
+    public function TotalIngEgre($anio, $mes){
+        //$totales = 0;
+        $caja = CajaChica::totalIngEgre($anio, $mes);
+        return $caja;
+
+    }
+
+
 }

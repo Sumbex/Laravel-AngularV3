41ab356e8de6d931ad4ede1ad4419ce9657573f2
davidluna1995 <david.luna.cuevas@gmail.com>
2019-07-26 21:36:49 -0400

    ac

--- laravel-module/public/app/styles.js
+++ laravel-module/public/app/styles.js
@@ -598,13 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD
 __webpack_require__(/*! C:\Users\david\Desktop\Laravel-AngularV3\angular-module\src\styles.css */"./src/styles.css");
 module.exports = __webpack_require__(/*! C:\Users\david\Desktop\Laravel-AngularV3\angular-module\node_modules\bootstrap\dist\css\bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
-=======
-__webpack_require__(/*! /Users/alejandroesteban/Desktop/cmpc_desarrollo/cmpc_dev/angular-module/src/styles.css */"./src/styles.css");
-module.exports = __webpack_require__(/*! /Users/alejandroesteban/Desktop/cmpc_desarrollo/cmpc_dev/angular-module/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
->>>>>>> alejandro
 
 
 /***/ })

70941b73f2cc35809e537a0f279daa836137ce96
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-07-29 14:30:00 -0400

    algo poco

--- laravel-module/public/app/styles.js
+++ laravel-module/public/app/styles.js
@@ -598,13 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-<<<<<<< HEAD:laravel-module/public/app/styles-es2015.js
 __webpack_require__(/*! /Users/alejandroesteban/Desktop/cmpc_desarrollo/cmpc_dev/angular-module/src/styles.css */"./src/styles.css");
 module.exports = __webpack_require__(/*! /Users/alejandroesteban/Desktop/cmpc_desarrollo/cmpc_dev/angular-module/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
-=======
-__webpack_require__(/*! /opt/lampp/htdocs/Laravel-AngularV3/angular-module/src/styles.css */"./src/styles.css");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/Laravel-AngularV3/angular-module/node_modules/bootstrap/dist/css/bootstrap.min.css */"./node_modules/bootstrap/dist/css/bootstrap.min.css");
->>>>>>> bryanv:laravel-module/public/app/styles.js
 
 
 /***/ })

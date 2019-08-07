5e3ac5bff42f348406e7cbd48662d84914b77389
Sumbex <bmontecino.96@gmail.com>
2019-08-07 11:16:36 -0400

    arreglo de jano

--- laravel-module/app/Cs_prestamos.php
+++ laravel-module/app/Cs_prestamos.php
@@ -925,20 +925,20 @@ class Cs_prestamos extends Model
 
                     if ($abonos['estado'] == 'success') {
                         $pasarPM = [];
-                        $pasarPM[0] = '';
+                        /* $pasarPM[0] = '';
                         $pasarPM[1] = '';
-                        $pasarPM[2] = '';
+                        $pasarPM[2] = ''; */
 
                         foreach ($abonos['abonos'] as $key) {
                             switch ($key->tipo) {
                                 case 1:
-                                    $pasarPM[0] = $key;
+                                    $pasarPM[] = $key;
                                     break;
                                 case 2:
-                                    $pasarPM[1] = $key;
+                                    $pasarPM[] = $key;
                                     break;
                                 case 3:
-                                    $pasarPM[2] = $key;
+                                    $pasarPM[] = $key;
                                     break;
                                 default:
                                     # code...
@@ -1211,20 +1211,20 @@ class Cs_prestamos extends Model
                         /* dd($abonos); */
                         if ($abonos['estado'] == 'success') {
                             $pasarPM = [];
-                            $pasarPM[0] = '';
+                            /* $pasarPM[0] = '';
                             $pasarPM[1] = '';
-                            $pasarPM[2] = '';
+                            $pasarPM[2] = ''; */
 
                             foreach ($abonos['abonos'] as $key) {
                                 switch ($key->tipo) {
                                     case 1:
-                                        $pasarPM[0] = $key;
+                                        $pasarPM[] = $key;
                                         break;
                                     case 2:
-                                        $pasarPM[1] = $key;
+                                        $pasarPM[] = $key;
                                         break;
                                     case 3:
-                                        $pasarPM[2] = $key;
+                                        $pasarPM[] = $key;
                                         break;
                                     default:
                                         # code...
--- laravel-module/public/app/main-es5.js
+++ laravel-module/public/app/main-es5.js
@@ -4946,7 +4946,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\bryan\Desktop\Proyecto CMCP\Laravel-AngularV3\angular-module\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\bryan\Desktop\Proyectos\Proyecto CMCP\Laravel-AngularV3\angular-module\src\main.ts */"./src/main.ts");
 
 
 /***/ })

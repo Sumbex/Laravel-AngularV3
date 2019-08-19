371ddce4b6b513a9f66dc0902d532e7701eeeaa3
bvidalneofox <v_andres01@hotmail.com>
2019-08-19 13:28:50 -0400

    cambio de authmaster para los select y login socio bloqueado

--- angular-module/src/app/public-master/login/login.component.ts
+++ angular-module/src/app/public-master/login/login.component.ts
@@ -47,7 +47,7 @@ export class LoginComponent implements OnInit {
         localStorage.setItem('token', JSON.stringify(this.token));
         localStorage.setItem('usuario', JSON.stringify(this.usuario.email));
         this.lockLogin = false;
-        this.router.navigate(['AuthMaster/CuentaSindical']);
+        this.router.navigate(['AuthMaster']);
         document.getElementById("closeModalLogin").click();
       }else{
         this.lockLogin = false;

af13cb00144018a88dfefa6028815e1e0ccadf13
Sumbex <bmontecino.96@gmail.com>
2019-08-22 16:24:41 -0400

    mejoramiento funcion validar rut

--- laravel-module/app/PortalSocio.php
+++ laravel-module/app/PortalSocio.php
@@ -348,27 +348,31 @@ class PortalSocio extends Authenticatable implements JWTSubject
 
     protected function validarRut($rut)
     {
-        $rut = preg_replace('/[^k0-9]/i', '', $rut);
-        $dv  = substr($rut, -1);
-        $numero = substr($rut, 0, strlen($rut) - 1);
-        $i = 2;
-        $suma = 0;
-        foreach (array_reverse(str_split($numero)) as $v) {
-            if ($i == 8)
-                $i = 2;
-            $suma += $v * $i;
-            ++$i;
-        }
-        $dvr = 11 - ($suma % 11);
+        try {
+            $rut = preg_replace('/[^k0-9]/i', '', $rut);
+            $dv  = substr($rut, -1);
+            $numero = substr($rut, 0, strlen($rut) - 1);
+            $i = 2;
+            $suma = 0;
+            foreach (array_reverse(str_split($numero)) as $v) {
+                if ($i == 8)
+                    $i = 2;
+                $suma += $v * $i;
+                ++$i;
+            }
+            $dvr = 11 - ($suma % 11);
 
-        if ($dvr == 11)
-            $dvr = 0;
-        if ($dvr == 10)
-            $dvr = 'K';
-        if ($dvr == strtoupper($dv))
-            return true;
-        else
+            if ($dvr == 11)
+                $dvr = 0;
+            if ($dvr == 10)
+                $dvr = 'K';
+            if ($dvr == strtoupper($dv))
+                return true;
+            else
+                return false;
+        } catch (\Exception $e) {
             return false;
+        }
     }
 
     protected function socioLogeado()

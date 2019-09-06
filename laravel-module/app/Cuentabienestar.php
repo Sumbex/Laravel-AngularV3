37a6943d3ecee3210679c0c2fc576c247da2153e
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-09-06 12:03:16 -0300

    subi9r al david

--- laravel-module/app/Cuentabienestar.php
+++ laravel-module/app/Cuentabienestar.php
@@ -10,12 +10,11 @@ class Cuentabienestar extends Model
     protected $table = "cuenta_bienestar";
 
 
-    protected function insertar_cuenta_gas($r)
+    protected function insertar_cuenta_sindical_gas($r)//aqui cae la del gas
     {
         $f = $this->div_fecha($r->fecha);
         $anio = $this->anio_tipo_id($f['anio']);
 
-       
 
         $verify = $this->where([
             'mes_id' => $f['mes'],
@@ -50,8 +49,61 @@ class Cuentabienestar extends Model
             return ['estado'=>'failed', 'mensaje'=>'Error al guardar item de cuenta del gas'];
         }
     }
+     protected function insertar_cuenta_sindical($r)//aqui cae la del gas, caja chica, reunion y votacion
+    {
+        $f = $this->div_fecha($r->fecha);
+        $anio = $this->anio_tipo_id($f['anio']);
+
+            $cbe = $this;
+            $cbe->anio_id = $anio->id;
+            $cbe->mes_id = $f['mes'];
+            $cbe->dia = $f['dia'];
+            $cbe->tipo_cuenta_bienestar_id = $r->tipo_cuenta_bienestar_id;
+            $cbe->numero_documento_1 = $r->numero_documento_1;
+            $cbe->archivo_documento_1 = 'documento.pdf' /*$r->archivo_documento_1*/;
+            $cbe->definicion = $r->definicion;
+            $cbe->activo = 'S';
+
+            if ($r->definicion == '1') { $cbe->monto_ingreso = $r->monto; }
+            if ($r->definicion == '2') { $cbe->monto_egreso = $r->monto; }
+
+            $cbe->descripcion = $r->descripcion;
+
+            if ($cbe->save()) {
+                return [ 'estado'=>'success', 'mensaje'=> 'Cuenta del gas ingresada correctamente' ];
+            }
+            return ['estado'=>'failed', 'mensaje'=>'Error al guardar item de cuenta del gas'];
+        
+    }
 
 
+    protected function listar($anio, $mes)
+    {
+        $list = DB::select("SELECT 
+                        cbe.id,
+                        concat(cbe.dia,' de ',m.descripcion,',',a.descripcion) as fecha,
+                        numero_documento_1,
+                        archivo_documento_1,
+                        monto_ingreso,
+                        monto_egreso,
+                        definicion,
+                        tcb.id as tipo_cuenta_bienestar_id,
+                        tcb.descripcion as tipo_cuenta,
+                        cbe.descripcion 
+                    from cuenta_bienestar cbe
+                    inner join anio as a on a.id = cbe.anio_id
+                    inner join mes as m on m.id = cbe.mes_id
+                    inner join tipo_cuenta_bienestar as tcb on tcb.id = cbe.tipo_cuenta_bienestar_id
+                    where cbe.mes_id = $mes and cbe.anio_id= $anio and cbe.activo ='S'
+                    --order by tcb.id ASC 
+                    order by tcb.id ASC , cbe.dia asc "
+        );
+        
+        if ($list > 0 ) {
+            return $list;
+        }
+        
+    }
 
     public function div_fecha($value)//funciona con input type date 
     {
--- laravel-module/app/Http/Controllers/CuentaBienestarController.php
+++ laravel-module/app/Http/Controllers/CuentaBienestarController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Cuentabienestar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class CuentaBienestarController extends Controller
 {
@@ -13,8 +15,20 @@ class CuentaBienestarController extends Controller
 
         switch ((string)$tipo_cuenta) {
             case '1': // cuenta del gas
-                $cuenta_gas = Cuentabienestar::insertar_cuenta_gas($r);
-                return $cuenta_gas;
+                $cuenta_be = Cuentabienestar::insertar_cuenta_sindical_gas($r);
+                return $cuenta_be;
+            break;
+            case '2': //inasistencia por reunion
+                 $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                return $cuenta_be;
+            break;
+            case '3': // inasistencia por votacion
+                 $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                return $cuenta_be;
+            break;
+            case '6': // detalle caja chica
+                 $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                return $cuenta_be;
             break;
             
             default:
@@ -22,5 +36,84 @@ class CuentaBienestarController extends Controller
                 break;
         }
 
+    }
+
+    public function listar_cuenta($anio, $mes)
+    {
+        $cm_txt = 'cbe_cierre_mensual';
+			
+		$c_m = DB::table($cm_txt)->where(['activo' => 'S','anio_id' => $anio,'mes_id' => $mes,
+		    	])->first();
+      
+        // dd(!empty($c_m->inicio_mensual));
+		if(!empty($c_m->inicio_mensual)){
+			$s_a = $c_m->inicio_mensual;
+            $tomar = true;
+
+            $listar = Cuentabienestar::listar($anio, $mes);
+
+            if ($listar > 0) {
+
+               
+                for ($i=0; $i < count($listar); $i++) { 
+                
+                    switch ($listar[$i]->definicion) {
+                        case '1':  
+                            if ($tomar == true) {
+                                $listar[$i]->saldo_actual_raw = $s_a + $listar[$i]->monto_ingreso;
+                                $tomar = false;
+
+                            }else{
+                                $listar[$i]->saldo_actual_raw = $listar[$i-1]->saldo_actual_raw  + $listar[$i]->monto_ingreso;
+                            }
+
+                        break;
+                        case '2':  
+                            if ($tomar == true) {
+                                $listar[$i]->saldo_actual_raw = $s_a - $listar[$i]->monto_egreso;
+                                $tomar = false;
+                                $ultimo_valor = $listar[$i]->saldo_actual_raw;
+
+                            }else{
+                                        
+                                $listar[$i]->saldo_actual_raw = $listar[$i-1]->saldo_actual_raw - $listar[$i]->monto_egreso;
+
+                            }
+                        break;
+                    
+                    }
+                    $return = [];
+                    $return['Cuenta_gas']=[];
+                    $return['inasistencia_reunion']=[];
+                    $return['caja_chica']=[];
+                    $return['inasistencia_votacion']=[];
+                    $return['fallecimiento']=[];
+                    $return['nacimiento']=[];
+                    $return['gastos_medicos']=[];
+
+                    foreach ($listar as $key) {
+                        switch ($key->tipo_cuenta_bienestar_id) {
+                            case '1': $return['Cuenta_gas'][] = $key; break;
+                            case '2': $return['inasistencia_reunion'][] = $key; break;
+                            case '3': $return['inasistencia_votacion'][] = $key; break;
+                            case '4': $return['fallecimiento'][] = $key; break;
+                            case '5': $return['nacimiento'][] = $key; break;
+                            case '6': $return['caja_chica'][] = $key; break;
+                            case '7': $return['gastos_medicos'][] = $key; break;
+                            
+                            default:
+                                # code...
+                                break;
+                        }
+                    }
+
+                    return $return;
+
+
+                }
+            }
+        }
+        return ['estado'=>'failed','mensaje'=>'No hay monto inicial en este mes'];
+       
     }
 }

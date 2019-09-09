9c391a472b69cb193329fcb879e72645de86da46
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-09-09 13:32:58 -0300

    guardando fallecidos, nacimiento

--- laravel-module/app/CbeFallecimiento.php
+++ laravel-module/app/CbeFallecimiento.php
@@ -8,11 +8,12 @@ class CbeFallecimiento extends Model
 {
     protected $table = 'cbe_fallecimiento';
 
-    protected function insertar($cbe_id, $socio_id)
+    protected function insertar($cbe_id, $socio_id, $rut)
     {
         $this->socio_id = $socio_id;
         $this->cuenta_bienestar_id = $cbe_id;
         $this->activo = 'S';
+        $this->rut_fallecido = $rut;
         if ($this->save()) {
             return [ 'estado'=>'success', 'mensaje'=> 'Item ingresado correctamente' ];
         }

f9237816a69336ec360c7b750b5713096f5305d8
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-09-16 15:20:22 -0300

    update bueno en cbe

--- laravel-module/app/Http/Controllers/CuentaBienestarController.php
+++ laravel-module/app/Http/Controllers/CuentaBienestarController.php
@@ -148,6 +148,49 @@ class CuentaBienestarController extends Controller
                     return $cuenta_be;
                 }
                 return $fall;
+			break;
+			
+			case '8': // 10% cuota socio
+                $validar = $this->validar_datos_bienestar($r);
+                if ($validar['estado'] == "success") {
+                    $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                    return $cuenta_be;
+                }
+                return $fall;
+			break;
+			
+			case '9': // Inasistencia por elecciones
+                $validar = $this->validar_datos_bienestar($r);
+                if ($validar['estado'] == "success") {
+                    $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                    return $cuenta_be;
+                }
+                return $fall;
+			break;
+			
+			case '10': // Consorcio
+                $validar = $this->validar_datos_bienestar($r);
+                if ($validar['estado'] == "success") {
+                    $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                    return $cuenta_be;
+                }
+                return $fall;
+			break;
+			case '11': // Cuota extraordinaria
+                $validar = $this->validar_datos_bienestar($r);
+                if ($validar['estado'] == "success") {
+                    $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                    return $cuenta_be;
+                }
+                return $fall;
+			break;
+			case '12': // No Sindicalizada
+                $validar = $this->validar_datos_bienestar($r);
+                if ($validar['estado'] == "success") {
+                    $cuenta_be = Cuentabienestar::insertar_cuenta_sindical($r);
+                    return $cuenta_be;
+                }
+                return $fall;
             break;
 
             //-------------------------------------------------------------------------------------------------
@@ -233,7 +276,13 @@ class CuentaBienestarController extends Controller
                     $return['caja_chica']=[];
                     $return['nacimiento']=[];
                     $return['fallecimiento']=[];
-                    $return['gastos_medicos']=[];
+					$return['gastos_medicos']=[];
+					
+					$return['socio_cuota']=[];
+					$return['inasistencia_elecciones']=[];
+					$return['consorcio']=[];
+					$return['cuota_extraordinaria']=[];
+					$return['no_sindicalizados']=[];
 
                     foreach ($listar as $key) {
                         switch ($key->tipo_cuenta_bienestar_id) {
@@ -243,7 +292,13 @@ class CuentaBienestarController extends Controller
                             case '6': $return['caja_chica'][] = $key; break;
                             case '5': $return['nacimiento'][] = $key; break;
                             case '4': $return['fallecimiento'][] = $key; break;
-                            case '7': $return['gastos_medicos'][] = $key; break;
+							case '7': $return['gastos_medicos'][] = $key; break;
+							
+							case '8': $return['socio_cuota'][] = $key; break;
+							case '9': $return['inasistencia_elecciones'][] = $key; break;
+							case '10': $return['consorcio'][] = $key;  break;
+							case '11': $return['cuota_extraordinaria'][] = $key;  break;
+							case '12': $return['no_sindicalizados'][] = $key;  break;
                             
                             default:
                                 # code...
@@ -313,7 +368,7 @@ class CuentaBienestarController extends Controller
 
 			break;
 			
-    		case 'tipo_cuenta_sindicato':
+    		case 'tipo_cuenta_bienestar':
     			
     			$mes =	$cs->mes_id;
     			$anio = $cs->anio_id;
@@ -391,45 +446,7 @@ class CuentaBienestarController extends Controller
 
 
     		break;
-    		case 'definicion_camping':
-    			if ($cs->definicion == $r->input) {
-    				return ['estado'=>'failed','mensaje'=>'Ya existe esta definiciósn'];
-    			}
-
-    			if ($r->input == 1) {
-    				$monto = $cs->monto_egreso;
-    				//dd("1: ".$monto);
-
-    				$cs->monto_ingreso = $monto;
-    				$cs->monto_egreso = null;
-    				$cs->definicion = $r->input;
-    				if ($cs->save()) {
-
-    					//$this->calcular_total_camping($cs);
-    					return ['estado'=>'success','mensaje'=>'Definición actualizada'];
-    				}
-    				return ['estado'=>'failed','mensaje'=>'error al actualizar'];
-    			}
-
-    			if ($r->input == 2) {
-    				$monto = $cs->monto_ingreso;
-    				//dd("2: ".$monto);
-
-    				$cs->monto_egreso = $monto;
-    				$cs->monto_ingreso = null;
-    				$cs->definicion = $r->input;
-    				if ($cs->save()) {
-
-    					//$this->calcular_total_camping($cs);
-
-    					return ['estado'=>'success','mensaje'=>'Definición actualizada'];
-    				}
-    				return ['estado'=>'failed','mensaje'=>'error al actualizar'];
-    			}
-    			
-
-
-    		break;
+    		
     		case 'monto':
     			
     			if ($cs->definicion == 1) {
@@ -449,18 +466,46 @@ class CuentaBienestarController extends Controller
 
     		break;
 
-    		case 'archivo':
+    		case 'archivo_1':
     			if($valida_pdf['estado'] == 'success'){
-    			 $ruta = substr($cs->archivo_documento, 8);
+    			 $ruta = substr($cs->archivo_documento_1, 8);
 				//$ruta = $cs->archivo_documento;
 					
                     $borrar = Storage::delete($ruta);
 
                     if ($borrar) {
-                        $guardarArchivo = $this->guardarArchivo($r->input, 'archivos_sindical/');
+                        $guardarArchivo = $this->guardarArchivo($r->input, 'archivos_bienestar/');
 
                         if ($guardarArchivo['estado'] == "success") {
-                            $cs->archivo_documento = 'storage/'.$guardarArchivo['archivo'];
+                            $cs->archivo_documento_1 = 'storage/'.$guardarArchivo['archivo'];
+                            if ($cs->save()) {
+                                return ['estado' => 'success', 'mensaje' => 'Archivo Modificado'];
+                            } else {
+                                return ['estado' => 'failed', 'mensaje' => 'Error al actualizar'];
+                            }
+                        } else {
+                            return $guardarArchivo;
+                        }
+                    } else {
+                        return ['estado' => 'failed', 'mensaje' => 'No se pudo actualizar el archivo'];
+                    }
+                }else{
+                	return ['estado' => 'failed', 'mensaje' => 'El archivo no es un PDF o no existe un formato'];
+                }
+
+			break;
+			case 'archivo_2':
+    			if($valida_pdf['estado'] == 'success'){
+    			 $ruta = substr($cs->archivo_documento_2, 8);
+				//$ruta = $cs->archivo_documento;
+					
+                    $borrar = Storage::delete($ruta);
+
+                    if ($borrar) {
+                        $guardarArchivo = $this->guardarArchivo($r->input, 'archivos_bienestar/defuncion/');
+
+                        if ($guardarArchivo['estado'] == "success") {
+                            $cs->archivo_documento_2 = 'storage/'.$guardarArchivo['archivo'];
                             if ($cs->save()) {
                                 return ['estado' => 'success', 'mensaje' => 'Archivo Modificado'];
                             } else {

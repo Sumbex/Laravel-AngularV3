a2b0bc6d8ca73d936c9d7846190448e910b99a24
Sumbex <bmontecino.96@gmail.com>
2019-09-23 11:53:40 -0300

    verificar si existe conyuge en cargas

--- laravel-module/routes/Rutas_api/bryan_socios_api.php
+++ laravel-module/routes/Rutas_api/bryan_socios_api.php
@@ -34,7 +34,7 @@ Route::get('/traer_camping_socio/{anio}/{mes}', 'PortalSocioController@TraerCamp
 Route::get('/traer_prestamos_socio', 'PortalSocioController@TraerPrestamos');
 Route::get('/traer_pagos_prestamos_socio/{id}/{tipo}', 'PortalSocioController@TraerPagosPrestamos');
 Route::get('/traer_abonos_prestamos_socio/{id}/{tipo}', 'PortalSocioController@TraerPagosAbonos');
-
+Route::get('/traer_nacimientos_socio', 'PortalSocioController@TraerNacimientos');
 //------------------------------------------>rutas portal socio mis beneficios
 
 //------------------------------------------------FUNCION TEST------------------------------------------------//

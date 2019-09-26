37a8274401c5240965bc5cef19be53115b019250
Sumbex <bmontecino.96@gmail.com>
2019-09-26 16:08:28 -0300

    totales mensuales consorcio

--- laravel-module/app/PortalSocioCuentaConsorcio.php
+++ laravel-module/app/PortalSocioCuentaConsorcio.php
@@ -67,7 +67,8 @@ class PortalSocioCuentaConsorcio extends Model
                     }
                 }
                 if ($ttrue) {
-                    return ['estado' => 'success', 'mensual' => $consorcio, 'anual' => $sum];
+                    $DSCE = $this->sumaDSCEMensual($anio);
+                    return ['estado' => 'success', 'mensual' => $consorcio, 'anual' => $sum, 'DSCE' => $DSCE['DSCE']];
                 } else {
                     return ['estado' => 'failed', 'mensaje' => 'No existen registros en el año ingresado.'];
                 }
@@ -76,6 +77,35 @@ class PortalSocioCuentaConsorcio extends Model
             }
         } else {
             return $verificarSocio;
+        }
+    }
+
+    protected function sumaDSCEMensual($anio)
+    {
+        $DS = DB::table('cuenta_consorcio')
+            ->select([
+                DB::raw("sum(coalesce(monto_mes_ds_1, 0) + coalesce(monto_mes_cex_1, 0)) as mes_1"),
+                DB::raw("sum(coalesce(monto_mes_ds_2, 0) + coalesce(monto_mes_cex_2, 0)) as mes_2"),
+                DB::raw("sum(coalesce(monto_mes_ds_3, 0) + coalesce(monto_mes_cex_3, 0)) as mes_3"),
+                DB::raw("sum(coalesce(monto_mes_ds_4, 0) + coalesce(monto_mes_cex_4, 0)) as mes_4"),
+                DB::raw("sum(coalesce(monto_mes_ds_5, 0) + coalesce(monto_mes_cex_5, 0)) as mes_5"),
+                DB::raw("sum(coalesce(monto_mes_ds_6, 0) + coalesce(monto_mes_cex_6, 0)) as mes_6"),
+                DB::raw("sum(coalesce(monto_mes_ds_7, 0) + coalesce(monto_mes_cex_7, 0)) as mes_7"),
+                DB::raw("sum(coalesce(monto_mes_ds_8, 0) + coalesce(monto_mes_cex_8, 0)) as mes_8"),
+                DB::raw("sum(coalesce(monto_mes_ds_9, 0) + coalesce(monto_mes_cex_9, 0)) as mes_9"),
+                DB::raw("sum(coalesce(monto_mes_ds_10, 0) + coalesce(monto_mes_cex_10, 0)) as mes_10"),
+                DB::raw("sum(coalesce(monto_mes_ds_11, 0) + coalesce(monto_mes_cex_11, 0)) as mes_11"),
+                DB::raw("sum(coalesce(monto_mes_ds_12, 0) + coalesce(monto_mes_cex_12, 0)) as mes_12")
+            ])
+            ->where([
+                'anio_id' => $anio
+            ])
+            ->get();
+
+        if (!$DS->isEmpty()) {
+            return ['estado' => 'success', 'DSCE' => $DS];
+        } else {
+            return ['estado' => 'failed', 'mensaje' => 'No existen registros en el año ingresado.'];
         }
     }
 }
--- laravel-module/app/PortalSocioMisBeneficios.php
+++ laravel-module/app/PortalSocioMisBeneficios.php
@@ -512,13 +512,87 @@ class PortalSocioMisBeneficios extends Model
 
         if (!$ahorro->isEmpty()) {
             $sum = 0;
+
             foreach ($ahorro[0] as $key) {
                 $sum = $sum + $key;
             }
-
+            $DS = $this->sumaDSMisAhorros($anio);
+            $CE = $this->sumaCEMisAhorros($anio);
             $total = $sum - $ahorro[0]->id;
 
-            return ['estado' => 'success', 'ahorro' => $ahorro, 'total' => $total];
+            return ['estado' => 'success', 'ahorro' => $ahorro, 'total' => $total, 'DS' => $DS['DS'], 'CE' => $CE['CE']];
+        } else {
+            return ['estado' => 'failed', 'mensaje' => 'Aun no tienes ahorros en el año ingresado.'];
+        }
+    }
+
+    protected function sumaDSMisAhorros($anio)
+    {
+        $DS = DB::table('cuenta_consorcio')
+            ->select([
+                DB::raw("coalesce(monto_mes_ds_1, 0) as mes_ds_1"),
+                DB::raw("coalesce(monto_mes_ds_2, 0) as mes_ds_2"),
+                DB::raw("coalesce(monto_mes_ds_3, 0) as mes_ds_3"),
+                DB::raw("coalesce(monto_mes_ds_4, 0) as mes_ds_4"),
+                DB::raw("coalesce(monto_mes_ds_5, 0) as mes_ds_5"),
+                DB::raw("coalesce(monto_mes_ds_6, 0) as mes_ds_6"),
+                DB::raw("coalesce(monto_mes_ds_7, 0) as mes_ds_7"),
+                DB::raw("coalesce(monto_mes_ds_8, 0) as mes_ds_8"),
+                DB::raw("coalesce(monto_mes_ds_9, 0) as mes_ds_9"),
+                DB::raw("coalesce(monto_mes_ds_10, 0) as mes_ds_10"),
+                DB::raw("coalesce(monto_mes_ds_11, 0) as mes_ds_11"),
+                DB::raw("coalesce(monto_mes_ds_12, 0) as mes_ds_12")
+            ])
+            ->where([
+                'vinculado' => 'S',
+                'anio_id' => $anio,
+                'socio_id' => $this->socioID()
+            ])
+            ->get();
+
+        if (!$DS->isEmpty()) {
+            $sum = 0;
+
+            foreach ($DS[0] as $key) {
+                $sum = $sum + $key;
+            }
+            return ['estado' => 'success', 'DS' => $sum];
+        } else {
+            return ['estado' => 'failed', 'mensaje' => 'Aun no tienes ahorros en el año ingresado.'];
+        }
+    }
+
+    protected function sumaCEMisAhorros($anio)
+    {
+        $DS = DB::table('cuenta_consorcio')
+            ->select([
+                DB::raw("coalesce(monto_mes_cex_1, 0) as mes_cex_1"),
+                DB::raw("coalesce(monto_mes_cex_2, 0) as mes_cex_2"),
+                DB::raw("coalesce(monto_mes_cex_3, 0) as mes_cex_3"),
+                DB::raw("coalesce(monto_mes_cex_4, 0) as mes_cex_4"),
+                DB::raw("coalesce(monto_mes_cex_5, 0) as mes_cex_5"),
+                DB::raw("coalesce(monto_mes_cex_6, 0) as mes_cex_6"),
+                DB::raw("coalesce(monto_mes_cex_7, 0) as mes_cex_7"),
+                DB::raw("coalesce(monto_mes_cex_8, 0) as mes_cex_8"),
+                DB::raw("coalesce(monto_mes_cex_9, 0) as mes_cex_9"),
+                DB::raw("coalesce(monto_mes_cex_10, 0) as mes_cex_10"),
+                DB::raw("coalesce(monto_mes_cex_11, 0) as mes_cex_11"),
+                DB::raw("coalesce(monto_mes_cex_12, 0) as mes_cex_12")
+            ])
+            ->where([
+                'vinculado' => 'S',
+                'anio_id' => $anio,
+                'socio_id' => $this->socioID()
+            ])
+            ->get();
+
+        if (!$DS->isEmpty()) {
+            $sum = 0;
+
+            foreach ($DS[0] as $key) {
+                $sum = $sum + $key;
+            }
+            return ['estado' => 'success', 'CE' => $sum];
         } else {
             return ['estado' => 'failed', 'mensaje' => 'Aun no tienes ahorros en el año ingresado.'];
         }

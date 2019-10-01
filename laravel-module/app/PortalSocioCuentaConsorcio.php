9d271d7235b6a2e55c62eece9f13f22279424f89
Sumbex <bmontecino.96@gmail.com>
2019-10-01 11:17:44 -0300

    cambios de sumas a restas

--- laravel-module/app/PortalSocioCuentaConsorcio.php
+++ laravel-module/app/PortalSocioCuentaConsorcio.php
@@ -67,7 +67,7 @@ class PortalSocioCuentaConsorcio extends Model
                     }
                 }
                 if ($ttrue) {
-                    $DSCE = $this->sumaDSCEMensual($anio);
+                    $DSCE = $this->restaDSCEMensual($anio);
                     return ['estado' => 'success', 'mensual' => $consorcio, 'anual' => $sum, 'DSCE' => $DSCE['DSCE']];
                 } else {
                     return ['estado' => 'failed', 'mensaje' => 'No existen registros en el año ingresado.'];
@@ -80,22 +80,22 @@ class PortalSocioCuentaConsorcio extends Model
         }
     }
 
-    protected function sumaDSCEMensual($anio)
+    protected function restaDSCEMensual($anio)
     {
         $DS = DB::table('cuenta_consorcio')
             ->select([
-                DB::raw("sum(coalesce(monto_mes_ds_1, 0) + coalesce(monto_mes_cex_1, 0)) as mes_1"),
-                DB::raw("sum(coalesce(monto_mes_ds_2, 0) + coalesce(monto_mes_cex_2, 0)) as mes_2"),
-                DB::raw("sum(coalesce(monto_mes_ds_3, 0) + coalesce(monto_mes_cex_3, 0)) as mes_3"),
-                DB::raw("sum(coalesce(monto_mes_ds_4, 0) + coalesce(monto_mes_cex_4, 0)) as mes_4"),
-                DB::raw("sum(coalesce(monto_mes_ds_5, 0) + coalesce(monto_mes_cex_5, 0)) as mes_5"),
-                DB::raw("sum(coalesce(monto_mes_ds_6, 0) + coalesce(monto_mes_cex_6, 0)) as mes_6"),
-                DB::raw("sum(coalesce(monto_mes_ds_7, 0) + coalesce(monto_mes_cex_7, 0)) as mes_7"),
-                DB::raw("sum(coalesce(monto_mes_ds_8, 0) + coalesce(monto_mes_cex_8, 0)) as mes_8"),
-                DB::raw("sum(coalesce(monto_mes_ds_9, 0) + coalesce(monto_mes_cex_9, 0)) as mes_9"),
-                DB::raw("sum(coalesce(monto_mes_ds_10, 0) + coalesce(monto_mes_cex_10, 0)) as mes_10"),
-                DB::raw("sum(coalesce(monto_mes_ds_11, 0) + coalesce(monto_mes_cex_11, 0)) as mes_11"),
-                DB::raw("sum(coalesce(monto_mes_ds_12, 0) + coalesce(monto_mes_cex_12, 0)) as mes_12")
+                DB::raw("sum(coalesce(monto_mes_ds_1, 0) - coalesce(monto_mes_cex_1, 0)) as mes_1"),
+                DB::raw("sum(coalesce(monto_mes_ds_2, 0) - coalesce(monto_mes_cex_2, 0)) as mes_2"),
+                DB::raw("sum(coalesce(monto_mes_ds_3, 0) - coalesce(monto_mes_cex_3, 0)) as mes_3"),
+                DB::raw("sum(coalesce(monto_mes_ds_4, 0) - coalesce(monto_mes_cex_4, 0)) as mes_4"),
+                DB::raw("sum(coalesce(monto_mes_ds_5, 0) - coalesce(monto_mes_cex_5, 0)) as mes_5"),
+                DB::raw("sum(coalesce(monto_mes_ds_6, 0) - coalesce(monto_mes_cex_6, 0)) as mes_6"),
+                DB::raw("sum(coalesce(monto_mes_ds_7, 0) - coalesce(monto_mes_cex_7, 0)) as mes_7"),
+                DB::raw("sum(coalesce(monto_mes_ds_8, 0) - coalesce(monto_mes_cex_8, 0)) as mes_8"),
+                DB::raw("sum(coalesce(monto_mes_ds_9, 0) - coalesce(monto_mes_cex_9, 0)) as mes_9"),
+                DB::raw("sum(coalesce(monto_mes_ds_10, 0) - coalesce(monto_mes_cex_10, 0)) as mes_10"),
+                DB::raw("sum(coalesce(monto_mes_ds_11, 0) - coalesce(monto_mes_cex_11, 0)) as mes_11"),
+                DB::raw("sum(coalesce(monto_mes_ds_12, 0) - coalesce(monto_mes_cex_12, 0)) as mes_12")
             ])
             ->where([
                 'anio_id' => $anio

0cea15b584c8280a03dae6db44334ff7129146f7
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-10-02 15:48:26 -0300

    cambios de alejandro

--- laravel-module/app/Socios.php
+++ laravel-module/app/Socios.php
@@ -29,5 +29,31 @@ class Socios extends Model
 						where lower(CONCAT(nombres,' ',a_paterno,' ',a_materno)) like  '%$busca%'");
 
     	return $filtro;
-    }
+	}
+	
+	protected function nombre_por_rut($rut, $socio)
+	{
+		$cl =  DB::table('cargas_legales_socio')
+		->select(['nombres','apellido_paterno','apellido_materno'])
+		->where(['rut'=>$rut, 'socio_id'=>$socio])->first();
+		$ben = DB::table('socio_beneficiario')
+		->select(['nombres','apellido_paterno','apellido_materno'])
+		->where(['rut'=>$rut, 'socio_id'=>$socio])->first();
+		$con =  DB::table('socio_conyuge')
+		->select(['nombres','apellido_paterno','apellido_materno'])
+		->where(['rut'=>$rut, 'socio_id'=>$socio])->first();
+		$ps =  DB::table('padres_suegros_socio')
+		->select(['nombres','apellido_paterno','apellido_materno'])
+		->where(['rut'=>$rut, 'socio_id'=>$socio])->first();
+
+
+		if ($cl) { return $cl->nombres.' '.$cl->apellido_paterno.' '.$cl->apellido_materno;  }
+		if ($ben) { return $ben->nombres.' '.$ben->apellido_paterno.' '.$ben->apellido_materno;  }
+		if ($con) { return $con->nombres.' '.$con->apellido_paterno.' '.$con->apellido_materno; }
+		if ($ps) {  return $ps->nombres.' '.$ps->apellido_paterno.' '.$ps->apellido_materno; }
+		return 'Sin nombre..';
+
+                                   
+
+	}
 }

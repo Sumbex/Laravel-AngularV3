56ed734920245260d50f6a9b067962336c4c651b
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2019-10-14 13:12:54 -0300

    kkck

--- laravel-module/app/Http/Controllers/CuentaConsorcioController.php
+++ laravel-module/app/Http/Controllers/CuentaConsorcioController.php
@@ -229,6 +229,11 @@ class CuentaConsorcioController extends Controller
     public function aplicar_descuento_dia_sueldo($socio_id, $porc, $desc, $mes, $anio, $monto=0)
     {
         try{
+            if ($monto == 0) {
+                return [ 'estado'=>'failed', 'mensaje'=>'Ingrese un monto para guardar' ];
+            }
+
+
             DB::beginTransaction();
             
             $sum =0;

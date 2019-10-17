0d7bd9a2abf23505904073613068d14ad5846d23
Sumbex <bmontecino.96@gmail.com>
2019-10-17 13:01:11 -0300

    desvinculados consorcio

--- laravel-module/app/SecReuniones.php
+++ laravel-module/app/SecReuniones.php
@@ -307,6 +307,35 @@ class SecReuniones extends Model
         }
     }
 
+    protected function filtrarSocio($reunion_id, $socio)
+    {
+        $buscar = mb_strtolower($socio);
+
+        $datos = DB::table('socios as s')
+            ->select([
+                's.id',
+                DB::raw("concat(s.nombres,' ',s.a_paterno,' ',s.a_materno) as nombre"),
+                'sea.descripcion as estado'
+            ])
+            ->join('sec_asistencia as sa', 'sa.socio_id', 's.id')
+            ->join('sec_estado_asistencia as sea', 'sea.id', 'sa.estado_asistencia_id')
+            ->where([
+                's.activo' => 'S',
+                's.fecha_egreso' => null,
+                'sa.reunion_id' => $reunion_id
+            ])
+            ->whereRaw("lower(concat(s.nombres,' ',s.a_paterno,' ',s.a_materno))', 'like', '%' . $buscar . '%")
+            /* ->where('lower(CONCAT(s.nombres,' ',s.a_paterno,' ',s.a_materno))', 'like', $buscar); */
+            /* ->whereRaw('lower(CONCAT(s.nombres,' ',s.a_paterno,' ',s.a_materno)) like'. . '%$busca%') */
+            ->get();
+        dd($datos);
+        if (!$datos->isEmpty()) {
+            return ['estado' => 'success', 'socio' => $datos];
+        } else {
+            return ['estado' => 'failed', 'mensaje' => 'El socio ya no se encuenta activo en el sindicato o no existe.'];
+        }
+    }
+
     protected function validarRut($rut)
     {
         try {

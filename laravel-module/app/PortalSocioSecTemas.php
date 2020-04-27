ffcaf616e2213b23343e26a2ecbc5f2b9743f73f
Sumbex <bmontecino.96@gmail.com>
2020-04-27 11:43:07 -0400

    arreglos

--- laravel-module/app/PortalSocioSecTemas.php
+++ laravel-module/app/PortalSocioSecTemas.php
@@ -43,7 +43,8 @@ class PortalSocioSecTemas extends Model
                 ->get();
             if (!$temas->isEmpty()) {
                 foreach ($temas as $key) {
-                    setlocale(LC_TIME, 'es');
+                    /* setlocale(LC_TIME, 'es'); */
+                    setlocale(LC_TIME, 'es_CL.UTF-8');
                     $key->fecha_inicio = Carbon::parse($key->fecha_inicio)->formatLocalized('%d de %B del %Y');
                 }
                 return ['estado' => 'success', 'temas' => $temas];

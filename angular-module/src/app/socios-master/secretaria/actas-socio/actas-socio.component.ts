ccb66e4c018118858c5d1e21e0e5438750d135a4
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2020-07-24 11:47:39 -0400

    subir desarrollo

--- angular-module/src/app/socios-master/secretaria/actas-socio/actas-socio.component.ts
+++ angular-module/src/app/socios-master/secretaria/actas-socio/actas-socio.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { global } from '../../../servicios/global';
+import { Anios } from 'src/app/modelos/anios.model';
+import { AniosService } from 'src/app/servicios/anios.service';
 
 @Component({
   selector: 'app-actas-socio',
@@ -18,42 +20,81 @@ export class ActasSocioComponent implements OnInit {
 
   token = localStorage.getItem('token').replace(/['"]+/g, '');
 
-  constructor(private modalService2: NgbModal, public _http: HttpClient) { 
+  selectAnio: Anios[] = [];
+  idAnioActual;
+  suc_res1 = false;
+  valorAnio: Anios = {
+    descripcion: ''
+  };
+  anio_load = true;
+
+  constructor(private modalService2: NgbModal, public _http: HttpClient, private _fechasService: AniosService) { 
     this.url = global.url;
   }
 
   ngOnInit() {
-    this.listar();
+
+    this.cargar_select();
+    
   }
 
   modal_detalle(modal) {
     this.modal_tabla = this.modalService2.open(modal, { size: 'xl' });
   }
 
-  listar() {
+  cargar_select() {
+    this.selectAnio = JSON.parse(localStorage.getItem('anios'));
+    //Cargar id del Año actual
+    this._fechasService.getAnioActual().subscribe(
+      response => {
+        this.idAnioActual = response;
+        this.valorAnio.descripcion = this.idAnioActual.id;
+        this.suc_res1 = true;
 
-    this._http.get(this.url + "socio_listar_acta", {
-      headers: new HttpHeaders(
-        {
-          'Authorization': 'Bearer' + this.token,
-          //'Content-Type': 'application/form-data'
+        // this.listo_para_listar(this.suc_res1, this.suc_res2);
+        this.listar(this.suc_res1, this.valorAnio.descripcion);
+      },
+      error => {
+        console.log(error);
+      }
+    )
+
+  }
+  
+
+  listar(res1, anio) {
+    if (res1 == true) {
+      this.anio_load = true;
+      this._http.get(this.url + "listar_acta/" + anio, {
+        headers: new HttpHeaders(
+          {
+            'Authorization': 'Bearer' + this.token,
+            //'Content-Type': 'application/form-data'
+          }
+        )
+      }).subscribe((val: { 'estado', 'data' }) => {
+        console.log(val);
+        if (val.estado == "success") {
+          this.actas = val.data;
+          this.anio_load = false;
+          console.log(this.anio_load);
+
         }
-      )
-    }).subscribe((val: { 'estado', 'data' }) => {
-      console.log(val);
-      if (val.estado == "success") {
-        this.actas = val.data;
+        if (val.estado == "false") {
+          this.anio_load = false;
+        }
+      }, response => {
+        console.log("POST call in error", response);
+      },
+        () => {
+          console.log("The POST observable is now completed.");
+        });
+    }
+  }
 
-      }
-      if (val.estado == "false") {
-
-      }
-    }, response => {
-      console.log("POST call in error", response);
-    },
-      () => {
-        console.log("The POST observable is now completed.");
-      });
+  changeAnio(evento) {
+  
+    this.listar(true, this.valorAnio.descripcion);
   }
 
 }
